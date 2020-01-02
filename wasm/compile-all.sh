f352860c6f52762460193c0f138e8b067a233842
Álvaro Castro Castilla <a@fourthbit.com>
2020-01-02 23:09:46 +0100

    Compile script

--- wasm/compile-all.sh
+++ wasm/compile-all.sh
@@ -1,9 +1,32 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+
+#########################################################
+# General config
+#########################################################
 
 if [ -z $1 ]; then
     echo "Please provide Gambit's absolute path as argument"
     exit
 fi
+
+WORKDIR=`pwd`
+OUTDIR="out"
+
+SCHEME_PROJ_DIR=".."
+PROJ_OUTDIR=$WORKDIR/$OUTDIR/scheme
+mkdir -p $PROJ_OUTDIR
+
+SCHEME_LIBS_DIR="../.."
+LIBS_OUTDIR=$WORKDIR/$OUTDIR/scheme-libs
+mkdir -p $LIBS_OUTDIR
+
+C_FLAGS="-Wno-incompatible-pointer-types-discards-qualifiers -Wno-incompatible-pointer-types"
+
+
+#########################################################
+# Gambit Runtime
+#########################################################
 
 GAMBIT_DIR=$1
 GAMBIT_LIB_DIR="$GAMBIT_DIR/lib"
@@ -43,57 +66,134 @@ COMP_LIB_PR="$COMP_GEN $FLAGS_OPT $DEFS_LIB $DEFS_SH -D___LIBRARY -D___PRIMAL"
 COMP_LIB_PR_RTS="$COMP_GEN $FLAGS_OPT_RTS $DEFS_LIB $DEFS_SH -D___LIBRARY -D___PRIMAL"
 COMP_APP="$COMP_GEN $FLAGS_OPT $DEFS_SH"
 
-$COMP_LIB_PR_RTS -o out/main.bc $GAMBIT_LIB_DIR/main.c
-$COMP_LIB_PR_RTS -o out/setup.bc $GAMBIT_LIB_DIR/setup.c
-$COMP_LIB_PR_RTS -o out/mem.bc $GAMBIT_LIB_DIR/mem.c
-$COMP_LIB_PR_RTS -o out/os_setup.bc $GAMBIT_LIB_DIR/os_setup.c
-$COMP_LIB_PR_RTS -o out/os_base.bc $GAMBIT_LIB_DIR/os_base.c
-$COMP_LIB_PR_RTS -o out/os_time.bc $GAMBIT_LIB_DIR/os_time.c
-$COMP_LIB_PR_RTS -o out/os_shell.bc $GAMBIT_LIB_DIR/os_shell.c
-$COMP_LIB_PR_RTS -o out/os_files.bc $GAMBIT_LIB_DIR/os_files.c
-$COMP_LIB_PR_RTS -o out/os_dyn.bc $GAMBIT_LIB_DIR/os_dyn.c
-$COMP_LIB_PR_RTS -o out/os_tty.bc $GAMBIT_LIB_DIR/os_tty.c
-$COMP_LIB_PR_RTS -o out/os_io.bc $GAMBIT_LIB_DIR/os_io.c
-$COMP_LIB_PR_RTS -o out/os_thread.bc $GAMBIT_LIB_DIR/os_thread.c
-$COMP_LIB_PR_RTS -o out/c_intf.bc $GAMBIT_LIB_DIR/c_intf.c
-$COMP_LIB_PR_RTS -o out/actlog.bc $GAMBIT_LIB_DIR/actlog.c
+GAMBIT_OUTDIR=out/gambit/
 
-$COMP_LIB_PR -o out/_kernel.bc $GAMBIT_LIB_DIR/_kernel.c
-$COMP_LIB_PR -o out/_system.bc $GAMBIT_LIB_DIR/_system.c
-$COMP_LIB_PR_MH -o out/_num.bc $GAMBIT_LIB_DIR/_num.c
-$COMP_LIB_PR -o out/_std.bc $GAMBIT_LIB_DIR/_std.c
-$COMP_LIB_PR -o out/_eval.bc $GAMBIT_LIB_DIR/_eval.c
-$COMP_LIB_PR -o out/_io.bc $GAMBIT_LIB_DIR/_io.c
-$COMP_LIB_PR -o out/_module.bc $GAMBIT_LIB_DIR/_module.c
-$COMP_LIB_PR -o out/_nonstd.bc $GAMBIT_LIB_DIR/_nonstd.c
-$COMP_LIB_PR -o out/_thread.bc $GAMBIT_LIB_DIR/_thread.c
-$COMP_LIB_PR -o out/_repl.bc $GAMBIT_LIB_DIR/_repl.c
+compile_gambit_runtime() {
+    echo "Compiling Gambit..."
 
-$COMP_LIB_PR -o out/_gambit.bc $GAMBIT_LIB_DIR/_gambit.c
+    $COMP_LIB_PR_RTS -c -o $GAMBIT_OUTDIR/main.bc $GAMBIT_LIB_DIR/main.c
+    $COMP_LIB_PR_RTS -c -o $GAMBIT_OUTDIR/setup.bc $GAMBIT_LIB_DIR/setup.c
+    $COMP_LIB_PR_RTS -c -o $GAMBIT_OUTDIR/mem.bc $GAMBIT_LIB_DIR/mem.c
+    $COMP_LIB_PR_RTS -c -o $GAMBIT_OUTDIR/os_setup.bc $GAMBIT_LIB_DIR/os_setup.c
+    $COMP_LIB_PR_RTS -c -o $GAMBIT_OUTDIR/os_base.bc $GAMBIT_LIB_DIR/os_base.c
+    $COMP_LIB_PR_RTS -c -o $GAMBIT_OUTDIR/os_time.bc $GAMBIT_LIB_DIR/os_time.c
+    $COMP_LIB_PR_RTS -c -o $GAMBIT_OUTDIR/os_shell.bc $GAMBIT_LIB_DIR/os_shell.c
+    $COMP_LIB_PR_RTS -c -o $GAMBIT_OUTDIR/os_files.bc $GAMBIT_LIB_DIR/os_files.c
+    $COMP_LIB_PR_RTS -c -o $GAMBIT_OUTDIR/os_dyn.bc $GAMBIT_LIB_DIR/os_dyn.c
+    $COMP_LIB_PR_RTS -c -o $GAMBIT_OUTDIR/os_tty.bc $GAMBIT_LIB_DIR/os_tty.c
+    $COMP_LIB_PR_RTS -c -o $GAMBIT_OUTDIR/os_io.bc $GAMBIT_LIB_DIR/os_io.c
+    $COMP_LIB_PR_RTS -c -o $GAMBIT_OUTDIR/os_thread.bc $GAMBIT_LIB_DIR/os_thread.c
+    $COMP_LIB_PR_RTS -c -o $GAMBIT_OUTDIR/c_intf.bc $GAMBIT_LIB_DIR/c_intf.c
+    $COMP_LIB_PR_RTS -c -o $GAMBIT_OUTDIR/actlog.bc $GAMBIT_LIB_DIR/actlog.c
 
-$COMP_LIB_PR out/main.bc \
-             out/setup.bc \
-             out/mem.bc \
-             out/os_setup.bc \
-             out/os_base.bc \
-             out/os_time.bc \
-             out/os_shell.bc \
-             out/os_files.bc \
-             out/os_dyn.bc \
-             out/os_tty.bc \
-             out/os_io.bc \
-             out/os_thread.bc \
-             out/c_intf.bc \
-             out/actlog.bc \
-             out/_kernel.bc \
-             out/_system.bc \
-             out/_num.bc \
-             out/_std.bc \
-             out/_eval.bc \
-             out/_io.bc \
-             out/_module.bc \
-             out/_nonstd.bc \
-             out/_thread.bc \
-             out/_repl.bc \
-             out/_gambit.bc \
-             -o out/libgambit.bc
+    $COMP_LIB_PR -c -o $GAMBIT_OUTDIR/_kernel.bc $GAMBIT_LIB_DIR/_kernel.c
+    $COMP_LIB_PR -c -o $GAMBIT_OUTDIR/_system.bc $GAMBIT_LIB_DIR/_system.c
+    $COMP_LIB_PR_MH -c -o $GAMBIT_OUTDIR/_num.bc $GAMBIT_LIB_DIR/_num.c
+    $COMP_LIB_PR -c -o $GAMBIT_OUTDIR/_std.bc $GAMBIT_LIB_DIR/_std.c
+    $COMP_LIB_PR -c -o $GAMBIT_OUTDIR/_eval.bc $GAMBIT_LIB_DIR/_eval.c
+    $COMP_LIB_PR_MH -c -o $GAMBIT_OUTDIR/_io.bc $GAMBIT_LIB_DIR/_io.c
+    $COMP_LIB_PR -c -o $GAMBIT_OUTDIR/_module.bc $GAMBIT_LIB_DIR/_module.c
+    $COMP_LIB_PR -c -o $GAMBIT_OUTDIR/_nonstd.bc $GAMBIT_LIB_DIR/_nonstd.c
+    $COMP_LIB_PR -c -o $GAMBIT_OUTDIR/_thread.bc $GAMBIT_LIB_DIR/_thread.c
+    $COMP_LIB_PR -c -o $GAMBIT_OUTDIR/_repl.bc $GAMBIT_LIB_DIR/_repl.c
+
+    $COMP_LIB_PR -c -o $GAMBIT_OUTDIR/_gambit.bc $GAMBIT_LIB_DIR/_gambit.c
+
+    $COMP_LIB_PR $GAMBIT_OUTDIR/main.bc \
+                 $GAMBIT_OUTDIR/setup.bc \
+                 $GAMBIT_OUTDIR/mem.bc \
+                 $GAMBIT_OUTDIR/os_setup.bc \
+                 $GAMBIT_OUTDIR/os_base.bc \
+                 $GAMBIT_OUTDIR/os_time.bc \
+                 $GAMBIT_OUTDIR/os_shell.bc \
+                 $GAMBIT_OUTDIR/os_files.bc \
+                 $GAMBIT_OUTDIR/os_dyn.bc \
+                 $GAMBIT_OUTDIR/os_tty.bc \
+                 $GAMBIT_OUTDIR/os_io.bc \
+                 $GAMBIT_OUTDIR/os_thread.bc \
+                 $GAMBIT_OUTDIR/c_intf.bc \
+                 $GAMBIT_OUTDIR/actlog.bc \
+                 $GAMBIT_OUTDIR/_kernel.bc \
+                 $GAMBIT_OUTDIR/_system.bc \
+                 $GAMBIT_OUTDIR/_num.bc \
+                 $GAMBIT_OUTDIR/_std.bc \
+                 $GAMBIT_OUTDIR/_eval.bc \
+                 $GAMBIT_OUTDIR/_io.bc \
+                 $GAMBIT_OUTDIR/_module.bc \
+                 $GAMBIT_OUTDIR/_nonstd.bc \
+                 $GAMBIT_OUTDIR/_thread.bc \
+                 $GAMBIT_OUTDIR/_repl.bc \
+                 $GAMBIT_OUTDIR/_gambit.bc \
+                 -o $OUTDIR/libgambit.bc
+
+    return 1
+}
+
+if [ ! -d $GAMBIT_OUTDIR ]; then
+    mkdir -p $GAMBIT_OUTDIR
+    compile_gambit_runtime
+fi
+
+
+#########################################################
+# Scheme Libraries
+#########################################################
+
+SCHEME_LIBS="
+base/alist/alist.sld
+base/functional/functional.sld
+base/functional/combinator/combinator.sld
+base/memoization/memoization.sld
+math/matrix/matrix.sld
+math/vector2/vector2.sld
+sdl2/sdl2.sld
+sdl2/ttf/ttf.sld
+gles2/gles2.sld
+"
+
+EMCC_LIB_FLAGS="-s USE_SDL=2 -s USE_SDL_TTF=2"
+
+for lib in $SCHEME_LIBS; do
+    echo "Compiling Scheme library" $lib "..."
+    SOURCE_DIR=`dirname $SCHEME_LIBS_DIR/$lib`
+    FILE=`basename $SCHEME_LIBS_DIR/$lib`
+    LOCAL_DIR=`dirname $lib`
+    TARGET_FILE=$(sed -E 's/(.sld|.scm)//g' <<< $LIBS_OUTDIR/$LOCAL_DIR/$FILE)
+    cd $SOURCE_DIR
+    gsc -c -ld-options '$CLANG_FLAGS' $FILE
+    mkdir -p $LIBS_OUTDIR/$LOCAL_DIR
+    cp ${FILE//.sld}.c $LIBS_OUTDIR/$LOCAL_DIR
+    emcc $EMCC_LIB_FLAGS -I$WORKDIR -c $TARGET_FILE.c $C_FLAGS -o $TARGET_FILE.bc
+    cd $WORKDIR
+done
+
+
+#########################################################
+# Main Project
+#########################################################
+
+SCHEME_PROJ_LIBS="
+core/core.sld
+render/render.sld
+main.scm
+"
+
+for lib in $SCHEME_PROJ_LIBS; do
+    echo "Compiling Scheme project file" $lib "..."
+    LOCAL_DIR=`dirname $lib`
+    TARGET_FILE=$(sed -E 's/(.sld|.scm)//g' <<< $PROJ_OUTDIR/$lib)
+    cd $SCHEME_PROJ_DIR
+    mkdir -p $PROJ_OUTDIR/$LOCAL_DIR
+    gsc -:search=./,search=.. -o $TARGET_FILE.c -c $lib
+    emcc -I$WORKDIR -c $TARGET_FILE.c $C_FLAGS -o $TARGET_FILE.bc
+    cd $WORKDIR
+done
+
+
+#########################################################
+# Final compilation
+#########################################################
+
+gsc -link -o $OUTDIR/app_.c $(find $PROJ_OUTDIR $LIBS_OUTDIR -iname "*.c" | tr "\n" " ")
+emcc -I$WORKDIR $OUTDIR/app_.c -c -o $OUTDIR/app_.bc
+emcc $EMCC_LIB_FLAGS -s WASM=0 $OUTDIR/libgambit.bc $OUTDIR/app_.bc $(find $PROJ_OUTDIR $LIBS_OUTDIR -iname "*.bc" | tr "\n" " ") -o app.html
